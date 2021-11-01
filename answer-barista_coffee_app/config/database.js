9ae8c2fbbf8e82392789ecc462bde53f3f137512
Laura-Alford <89553183+Laura-Alford@users.noreply.github.com>
2021-10-31 23:08:41 -0400

    Add files via upload

---
+++ answer-barista_coffee_app/config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://la:LATest@cluster0.gneh9.mongodb.net/barista?retryWrites=true&w=majority', 
+    'dbName': 'barista'
+};
+//sends object
